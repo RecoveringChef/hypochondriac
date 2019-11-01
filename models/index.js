537ddf3466fd682ff442175dd12fdbdedbb8f2ed
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-11-01 14:16:27 -0400

    models/index.js updated

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
 module.exports = {
   Account: require("./account")
 };
+
+module.exports = {
+  Account: require("./news")
+};
+
+module.exports = {
+  Account: require("./symptoms")
+};
+
+module.exports = {
+  Account: require("./conditions")
+};
