2c47b7e8df798bc59fb14cfd4ebac48846c7109c
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-11-01 15:14:49 -0400

    tested register and log in functions as working

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,8 +5,8 @@ const userRoutes = require("./users");
 //User Routes
 router.use("/users", userRoutes);
 // For anything else, render the html page
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../../client/public/index.html"));
 });
 
 module.exports = router;
