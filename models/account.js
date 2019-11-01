415a8cafb364cd38216f4b59ff54e6ea06e15a83
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-11-01 13:02:08 -0400

    Readme

--- models/account.js
+++ models/account.js
@@ -4,7 +4,8 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const Account = new Schema({
     username: String,
-    password: String
+    password: String,
+    email: String
 });
 
 Account.plugin(passportLocalMongoose);
