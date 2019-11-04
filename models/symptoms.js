9cb6b97a8bad8e0f27a072de4c18216e90d83fd1
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-11-04 12:01:19 -0500

    quick typo fix i noticed before lunch

--- models/symptoms.js
+++ models/symptoms.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const Symptom = new Schema({
+const Symptoms = new Schema({
     name: { type: String, required: true }
 
 });
 
 
-module.exports = mongoose.model('symptoms', Symptom);
+module.exports = mongoose.model('symptoms', Symptoms);
